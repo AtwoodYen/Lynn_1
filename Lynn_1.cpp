be00b0bff7402217a42d80dac237cc555884baf0
Lynnviolin1210 <s-wanglyn@bsd405.org>
2025-10-22 21:17:19 -0700

    Update Lynn_1.cpp
    slight modification of code!!!!:DDD

--- Lynn_1.cpp
+++ Lynn_1.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
+    int ans=0
+    int n;
+    cin>>n;
     cout << "Hello, World!" << endl;
+    for (int x=0;x<n:x=x=+1)
+     {
+        ans=ans+x;
+    }
+    cout<<ans;
     return 0;
 }
